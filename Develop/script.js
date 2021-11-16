6a6460ff03ed9e41e010203ab51d4447fc755ab8
Sodaman <mattrick2214@gmail.com>
2021-11-16 14:51:57 -0600

    Adjusted css header and added slight adjustments to javascript.

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,27 @@
+window.alert("This is an alert! JavaScript is running!");
 // Assignment code here
 
+var pwLength = prompt ("How long would you like your password to be? (8-128 characters.)")
+  if (pwLength <= 128 && pwLength >= 8) {
+    console.log ("Pass.")
+  } else {
+    window.alert("Does not meet requirements!")
+  }
+function generatePassword () {
+  return "Test."
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+  var password = generatePassword ()
+;
+
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
@@ -15,3 +29,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+window.alert("Please...please....PLEASE!!!")
