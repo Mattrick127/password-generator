6775d93b58fc3b002e34dfb5798dbe785d284977
Sodaman <mattrick2214@gmail.com>
2021-11-19 15:27:58 -0600

    Added comment codes and condensed script file.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.alert("This is an alert! JavaScript is running!");
 // Assignment code here
 var passwordChars = "";
+// This is to check when user has answered no to all prompts.
 var check = false
 function writePassword() {
   // Ask for password length.
@@ -9,28 +10,24 @@ if (pwLength <= 128 && pwLength >= 8) {
 if (passwordChars != "") {
   passwordChars = ""
 }
+//Ask for lower case
 var lowerCase = prompt ("Would you like to include lower case characters? (a-z) YES or NO")
 // In case user does not comply 
 } else {
   return window.alert("Does not meet requirements!")
 }
 console.log (pwLength)
-////////////////////////////
-//Ask for lower case
 if (lowerCase === "YES"|| lowerCase === "yes") {
   check = true
   passwordChars += "abcdefghijklmnopqrstuvwxyz"
 }
 console.log (passwordChars)
-////////////////////////////
 //Ask for special case
 var specialCase = prompt ("Would you like to include special characters? (@-&) YES or NO")
-
 if (specialCase === "YES"|| specialCase === "yes") {
   check = true
-  passwordChars += "#@!%&()/"
+  passwordChars += " !#$%&'()*+,-./:;<=>?@]^_[`{|}~ "
 } console.log (passwordChars)
-////////////////////////////
 //Ask for upper case
 var upperCase = prompt ("Would you like to include upper case characters? (A-Z) YES or NO")
 
@@ -38,11 +35,8 @@ if (upperCase === "YES"|| upperCase === "yes") {
   check = true
   passwordChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 } console.log (passwordChars)
-
-////////////////////////////
 //Ask for numbers
 var numbers = prompt ("Would you like to include numbers? (0-9) YES or NO")
-
 if (numbers === "YES"|| numbers === "yes") {
   check = true
   passwordChars += "0123456789"
@@ -52,30 +46,23 @@ if (numbers === "YES"|| numbers === "yes") {
   passwordChars = ""
 }
 
-///////////////////////////
   var password = generatePassword (pwLength)
 ;
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
-// 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@!%&()/
-
+// This is the function to receive the password at it's length, and selected characters.
 function generatePassword (pwLength) {
-    var randomPassword = Array(pwLength)
+    var Password = Array(pwLength)
       .fill(passwordChars)
       .map(function (x) {
         return x[Math.floor(Math.random() * x.length)];
       })
       .join('');
-    return randomPassword;
+    return Password;
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-window.alert("Please...please....PLEASE!!!")
